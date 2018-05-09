0ce44d1acac105b1bbd29282556a7b94db270555
Andrei Radchenko <d4100kx@gmail.com>
2018-05-09 18:15:23 +0300

    Implement scan WiFi and BroadcastResiever, disconnect socet at stop, and reconnect at start MainActivity

--- app/src/main/java/unidesign/photo360/MainActivity.kt
+++ app/src/main/java/unidesign/photo360/MainActivity.kt
@@ -1,7 +1,13 @@
 package unidesign.photo360
 
+import android.Manifest
 import android.app.ProgressDialog
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.net.wifi.ScanResult
 import android.net.wifi.WifiConfiguration
 import android.net.wifi.WifiInfo
 import android.net.wifi.WifiManager
@@ -10,7 +16,9 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import android.os.Build
 import android.os.Handler
+import android.provider.Contacts
 import android.support.annotation.RequiresApi
+import android.support.annotation.UiThread
 import android.support.design.R.id.visible
 import android.support.v4.view.ViewPager
 import android.support.v7.widget.Toolbar
@@ -21,6 +29,7 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.*
 import kotlinx.android.synthetic.main.fragment_page.*
+import kotlinx.coroutines.experimental.*
 //import com.sun.org.apache.xml.internal.serializer.utils.Utils.messages
 import org.java_websocket.WebSocket
 
@@ -32,6 +41,9 @@ import org.java_websocket.drafts.Draft_17
 import org.java_websocket.drafts.Draft_6455
 import org.java_websocket.exceptions.WebsocketNotConnectedException
 import org.json.JSONObject
+import kotlin.coroutines.experimental.CoroutineContext
+import kotlinx.coroutines.experimental.android.UI
+import java.util.concurrent.TimeUnit
 
 
 class MainActivity : AppCompatActivity() {
@@ -42,9 +54,9 @@ class MainActivity : AppCompatActivity() {
 //    var networkSSID = "test"
 //    var networkPass = "pass"
     lateinit var wifiConf: WifiConfiguration
-    lateinit var wifiManager: WifiManager
+//    public lateinit var wifiManager: WifiManager
     //public lateinit var sharedPrefs: PreferenceManager
-    lateinit var homeWiFiInfo: WifiInfo
+    var homeWiFiInfo: WifiInfo? = null
     lateinit var btnRunCW: Button
     lateinit var btnRunCCW: Button
     lateinit var btnSTOP: Button
@@ -52,11 +64,17 @@ class MainActivity : AppCompatActivity() {
     lateinit var turntableView: TurntableView
     lateinit var mViewPager: ViewPager
     lateinit var pageAdapter: PageAdapter
+    lateinit var TurntableConectionJob: Job
+    private lateinit var wifiScanReceiver: BroadcastReceiver
+    //represents a common pool of shared threads as the coroutine dispatcher
+    private val bgContext: CoroutineContext = CommonPool
     //public val sharedPrefs = PreferenceManager(applicationContext)
     //public val sharedPrefs = PreferenceManager(applicationContext)
     companion object {
         var sharedPrefs: PreferenceManager? = null
         var currentFragmentId: Int = 0
+        lateinit var wifiManager: WifiManager
+        var fireWiFiScan = false
     }
 
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
@@ -137,6 +155,33 @@ class MainActivity : AppCompatActivity() {
 //        var currentFragment = pageAdapter.getItem(mViewPager.currentItem)
 //        framesLeftTxt = currentFragment.frames_left_txt
 //        framesLeftTxt.setText(sharedPrefs.frame)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if(checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            {
+                requestPermissions(Array<String>(1){Manifest.permission.ACCESS_COARSE_LOCATION}, 87);
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onStart() {
+        super.onStart()
+        TurntableConectionJob = Job()
+        wifiScanReceiver = WifiScanReceiver()
+        registerReceiver(wifiScanReceiver, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
+        connectWebSocket()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(wifiScanReceiver)
+        disconnectWebSocket()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        TurntableConectionJob.cancel()
     }
 
     private fun disableButton() {
@@ -167,17 +212,18 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.action_connect -> {
                 if (!(mWebSocketClient?.isOpen ?: false)) {
-                    /*                Snackbar.make(findViewById(R.id.ussd_toolbar), "Replace done with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+                    //if no WiFi connection allowed
+                    if (!wifiManager.isWifiEnabled){
+                        Toast.makeText(application, "Please, enable WiFi connection", Toast.LENGTH_SHORT).show()
+                    }
+                    else {
+                        menu?.getItem(0)?.setIcon(applicationContext.getDrawable(R.drawable.ic_action_connected))
+                        mprogresBar.visibility = View.VISIBLE
+                        wifiManager.startScan()
+                        fireWiFiScan = true
+                    }
 
-                    if (name.length == 0 && template.length == 0) {
-                        Snackbar.make(findViewById(R.id.ussd_toolbar), R.string.snackbar_fill_form, Snackbar.LENGTH_LONG)
-                                .setAction("Action", null).show()
-                        return false
-                    }*/
-                    menu?.getItem(0)?.setIcon(applicationContext.getDrawable(R.drawable.ic_action_connected));
-                    mprogresBar.visibility = View.VISIBLE
-                    connect2Photo360WiFi(sharedPrefs?.wifiSsid, sharedPrefs?.wifiPassword)
+                    //connect2Turntable()
 //                    connectWebSocket()
                 }
                 else {
@@ -202,7 +248,47 @@ class MainActivity : AppCompatActivity() {
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    private fun connect2Photo360WiFi(networkSSID: String?, networkPass: String?) {
+    fun connect2Turntable(){
+        TurntableConectionJob = launch(UI){
+            try {
+                val result1 = withContext(bgContext) { connect2Photo360WiFi(sharedPrefs?.wifiSsid, sharedPrefs?.wifiPassword) }
+
+                when (result1){
+                    "wifi connected" -> {
+                        Log.d("connect2Photo360WiFi", "[${Thread.currentThread().name}] " +"wifi connected")
+                        async(bgContext) {connectWebSocket()}
+                        //mprogresBar.visibility = View.INVISIBLE
+                        //Toast.makeText(application, "Please, enable WiFi connection", Toast.LENGTH_SHORT).show()
+                    }
+                    "wifi just connected" -> {
+                        Log.d("connect2Photo360WiFi", "[${Thread.currentThread().name}] " +"wifi just connected")
+                        async(bgContext) {
+                            delay(4000)
+                            connectWebSocket()}
+                        //Toast.makeText(application, "Please, enable WiFi connection", Toast.LENGTH_SHORT).show()
+                    }
+                    "wifi not added" -> {
+                        Log.d("connect2Photo360WiFi", "[${Thread.currentThread().name}] " +"wifi not found")
+                        Toast.makeText(application, "Turntable WiFi not added", Toast.LENGTH_SHORT).show()
+                        menu?.getItem(0)?.setIcon(applicationContext.getDrawable(R.drawable.ic_action_connect))
+                        mprogresBar.visibility = View.INVISIBLE
+                    }
+                }
+
+            } catch (exception: Exception) {
+                exception.printStackTrace()
+                mprogresBar.visibility = View.INVISIBLE
+            }
+
+            finally {
+                //Turn off busy indicator.
+
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private suspend fun connect2Photo360WiFi(networkSSID: String?, networkPass: String?): String {
         //TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
         homeWiFiInfo = wifiManager.connectionInfo
         wifiConf.SSID = "\"" + networkSSID + "\""
@@ -210,29 +296,28 @@ class MainActivity : AppCompatActivity() {
         wifiConf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
         wifiManager.addNetwork(wifiConf)
 
-        //if no WiFi connection allowed
-        if (!wifiManager.isWifiEnabled) {
-            Toast.makeText(application, "Please, enable WiFi connection", Toast.LENGTH_SHORT).show()
-            mprogresBar.visibility = View.INVISIBLE
-            menu?.getItem(0)?.setIcon(getDrawable(R.drawable.ic_action_connect))
-            return
-        }
-
-        Toast.makeText(application, "Connect to turntable", Toast.LENGTH_SHORT).show()
-        var list: List<WifiConfiguration> = wifiManager.getConfiguredNetworks()
-        for(  i: WifiConfiguration in list ) {
-             if(i.SSID != null && i.SSID.equals("\"" + networkSSID + "\"")) {
-                 wifiManager.disconnect()
-                 wifiManager.enableNetwork(i.networkId, true)
-                 wifiManager.reconnect()
-                 //wifiManager.connectionInfo.ssid
-                 break;
+        if (wifiManager.connectionInfo.ssid.equals(wifiConf.SSID))
+            return "wifi connected"
+        else {
+            //Toast.makeText(application, "Connect to turntable", Toast.LENGTH_SHORT).show()
+            var list: List<WifiConfiguration> = wifiManager.configuredNetworks
+            var scannedWiFi = wifiManager.scanResults
+            for (i: WifiConfiguration in list) {
+                        if (i.SSID != null && i.SSID.equals("\"" + networkSSID + "\"")) {
+                            wifiManager.disconnect()
+                            wifiManager.enableNetwork(i.networkId, true)
+                            wifiManager.reconnect()
+                            //Log.d("connect2Photo360WiFi", "[${Thread.currentThread().name}] " +"wifiManager.reconnect(): true")
+                            return "wifi just connected"
+                            break;
+                        }
             }
+            return "wifi not added"
         }
-        Handler().postDelayed(Runnable {
-            // do something...
-            connectWebSocket()
-        }, 4000)
+//        Handler().postDelayed(Runnable {
+//            // do something...
+//            connectWebSocket()
+//        }, 4000)
     }
 
     private fun disconnectFromPhoto360WiFi() {
@@ -245,7 +330,7 @@ class MainActivity : AppCompatActivity() {
 
         var list: List<WifiConfiguration> = wifiManager.getConfiguredNetworks()
         for(  i: WifiConfiguration in list ) {
-            if(i.SSID != null && i.SSID.equals(homeWiFiInfo.ssid)) {
+            if(i.SSID != null && i.SSID.equals(homeWiFiInfo?.ssid)) {
                 wifiManager.disconnect()
                 wifiManager.enableNetwork(i.networkId, true)
                 wifiManager.reconnect()
@@ -270,6 +355,7 @@ class MainActivity : AppCompatActivity() {
             override fun onOpen(serverHandshake: ServerHandshake) {
                 Log.i("Websocket", "Opened")
                 runOnUiThread {
+                    menu?.getItem(0)?.setIcon(applicationContext.getDrawable(R.drawable.ic_action_connected))
                     mprogresBar.visibility = View.INVISIBLE
                     Toast.makeText(application, "Turntable connected", Toast.LENGTH_SHORT).show()
                     enableButton()
@@ -316,6 +402,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+        Log.d("connectWebSocket", "[${Thread.currentThread().name}] " +"connect to web socket")
         mWebSocketClient?.connect()
         return true
     }
@@ -329,4 +416,30 @@ class MainActivity : AppCompatActivity() {
         mWebSocketClient!!.send(editText.text.toString())
         editText.setText("")
     }
+
+    inner class WifiScanReceiver: BroadcastReceiver() {
+        @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+        override fun onReceive(context: Context, intent: Intent) {
+            if(intent.getAction().equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION) && fireWiFiScan) {
+                fireWiFiScan = false
+                var scanResults = MainActivity.wifiManager.scanResults
+                var TurntableFound = false
+                for (i:ScanResult in scanResults){
+                    Log.d("WifiScanReceiver", "ScanResult: " + i.SSID)
+                    if (i.SSID.equals(sharedPrefs?.wifiSsid)) {
+                        connect2Turntable()
+                        TurntableFound = true
+                        break
+                    }
+                }
+                if (!TurntableFound){
+                    Toast.makeText(application, "Turntable WiFi not found", Toast.LENGTH_SHORT).show()
+                    menu?.getItem(0)?.setIcon(applicationContext.getDrawable(R.drawable.ic_action_connect))
+                    mprogresBar.visibility = View.INVISIBLE
+                }
+            }
+        }
+    }
 }
+
+
