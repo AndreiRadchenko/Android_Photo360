698b685cf4cc7cfb4fecb08f41d06ec6b598886b
andreiradchenko <d4100kx@gmail.com>
2018-09-06 09:13:53 +0300

    add working external link from help to 3dprint.com.ua

--- app/src/main/java/unidesign/photo360/MyWebViewClient.kt
+++ app/src/main/java/unidesign/photo360/MyWebViewClient.kt
@@ -1,7 +1,10 @@
 package unidesign.photo360
 
 
+import android.annotation.TargetApi
 import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.util.Log
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
@@ -9,9 +12,9 @@ import android.webkit.WebViewClient
 
 class MyWebViewClient : WebViewClient() {
 
+    @TargetApi(Build.VERSION_CODES.N)
     override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-
-        Log.d("MyWebViewClient", request.url.toString())
+        /*        Log.d("MyWebViewClient", request.url.toString())
         if (request.url.toString().contains("android_asset")) {
             // This is my web site, so do not override; let my WebView load the page
 
@@ -21,6 +24,34 @@ class MyWebViewClient : WebViewClient() {
         val intent = Intent(Intent.ACTION_VIEW, request.url)
         view.context.startActivity(intent)
 
-        return true
+        return true*/
+        val uri = request.url
+        return handleUri(uri, view);
     }
+
+    @SuppressWarnings("deprecation")
+    override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+        val uri = Uri.parse(url)
+        return handleUri(uri, view);
+    }
+
+    private fun handleUri(uri: Uri, view: WebView): Boolean {
+        Log.d("MyWebViewClient", "Uri =" + uri);
+        val host = uri.getHost();
+        val scheme = uri.getScheme();
+        // Based on some condition you need to determine if you are going to load the url
+        // in your web view itself or in a browser.
+        // You can use `host` or `scheme` or any part of the `uri` to decide.
+        if (uri.toString().contains("android_asset")) {
+            // Returning false means that you are going to load this url in the webView itself
+            return false;
+        } else {
+            // Returning true means that you need to handle what to do with the url
+            // e.g. open web page in a Browser
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            view.context.startActivity(intent)
+            return true;
+        }
+    }
+
 }
